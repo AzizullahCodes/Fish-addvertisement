f6643c0ebed3737db2a6fc503c8c74c3d9af2350
AzizullahCodes <azizullahaziz7777@gmail.com>
2025-04-01 07:18:09 +0500

    g

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-function callNow(){window.location.href ="tel:+92 03347257178"}
-function whatsappNow(){window.location.href ="https://wa.me/+92 03347257178";}
+// function callNow(){window.location.href ="tel:+92 03347257178"}
+// function whatsappNow(){window.location.href ="https://wa.me/+92 03347257178";}
